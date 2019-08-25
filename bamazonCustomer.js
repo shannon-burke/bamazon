9fd0bbbfd108ee79247f9cee3209b8e3bd91009b
Shannon <shannonburke.art@gmail.com>
2019-08-24 22:02:23 -0500

    completed JS

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,10 +19,53 @@ connection.connect(function (err) {
 
 function listItems() {
     connection.query("SELECT * FROM products", function (err, res) {
-
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("★===▼・ᴥ・▼===★===^･ω･^===★")
+        questions();
     })
 };
 
 function questions() {
+    inquirer.prompt([{
+        type: "input",
+        message: "Which item would you like to buy? (please enter ID)",
+        name: "itemID"
+    },
+    {
+        type: "input",
+        message: "How much ya want?",
+        name: "unitsBuy"
+    }]).then(function (inquirerResponse) {
+        var item = inquirerResponse.itemID - 1
 
+        connection.query("SELECT * FROM products", function (err, res) {
+            if (err) throw err;
+            var quantity = res[item].stock_quantity - inquirerResponse.unitsBuy;
+            var product = res[item].product_name
+
+            if (res[item].stock_quantity >= inquirerResponse.unitsBuy) {
+                var query = connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        { stock_quntity: quantity },
+                        { item_id: item }
+                    ],
+                    function (err, res) {
+                        console.log("You bought " + inquirerResponse.unitsBuy + " " + product + "(s)! Thanks!");
+                    });
+            }
+
+            else if (res[item].stock_quantity < inquirerResponse.unitsBuy) {
+                console.log("Looks like we don't have quite that much. Bummer.")
+            }
+
+            else {
+                console.log("Uhhhhhh... Oops?");
+            }
+        });
+        listItems();
+    });
 };
